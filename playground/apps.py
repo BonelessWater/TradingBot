a2196bbcf301f89026385a5a8e1c7ce54fba5b6a
Dom <112484879+BonelessWater@users.noreply.github.com>
2024-07-04 23:38:34 -0400

    Changed scheduler to stop running on ready

--- playground/apps.py
+++ playground/apps.py
@@ -3,30 +3,37 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 import logging
 
-#logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 class PlaygroundConfig(AppConfig):
     name = 'playground'
     verbose_name = "Playground Application"
 
     def ready(self):
-        from .tasks import save_data, save_all_sp500_metrics  # Import your task function here
-        self.start_scheduler(save_data)
-        self.start_scheduler(save_all_sp500_metrics)
+        self.start_scheduler()
 
-    def start_scheduler(self, task_function):
+    def start_scheduler(self):
+        from .tasks import save_data, save_all_sp500_metrics  # Import your task functions here
+
         scheduler = BackgroundScheduler()
+        
+        # Schedule save_data task to run daily at midnight
         scheduler.add_job(
-            task_function,
-            trigger=CronTrigger(hour=0, minute=0),  # Run daily at 12:00 M
-            id='daily_data_save',
+            save_data,
+            trigger=CronTrigger(hour=0, minute=0),  # Run daily at 12:00 AM
+            id='daily_save_data',
             replace_existing=True,
             max_instances=1
         )
+        
+        # Schedule save_all_sp500_metrics task to run daily at midnight
         scheduler.add_job(
-            task_function,
-            trigger='date',  # Run once at startup
-            id='startup_data_save'
+            save_all_sp500_metrics,
+            trigger=CronTrigger(hour=0, minute=0),  # Run daily at 12:00 AM
+            id='daily_save_all_sp500_metrics',
+            replace_existing=True,
+            max_instances=1
         )
+        
         scheduler.start()
-        #logger.debug("Schedulers have been started for data saving.")
+        logger.debug("Schedulers have been started for daily tasks.")
